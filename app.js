f255634d58f37afcb02acbaffd204698e432668e
Rujuan Xing <rXing@cs.mum.edu>
2018-03-19 22:11:11 -0500

    init version 1.0

---
+++ app.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var compression = require('compression');
+var helmet = require('helmet');
+
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://library:Tina1234@ds117759.mlab.com:17759/local_library';
+
+// var mongoDB = 'mongodb://localhost/local_library';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var lessMiddleware = require('less-middleware');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var catalog = require('./routes/catalog');
+
+var app = express();
+
+app.use(helmet());
+app.use(compression()); //Compress all routes
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(lessMiddleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+app.use('/catalog', catalog);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
